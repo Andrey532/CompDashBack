023c0468e674510437708bdbf65e253eac9b8f30
Andrii <andreygalushka30@gmail.com>
2024-10-04 19:37:48 +0300

    commit

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,17 +10,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('HOST'),
-        port: configService.get<number>('PORT'),
-        username: configService.get<string>('USER'),
-        password: configService.get<string>('PASSWORD'),
-        database: configService.get<string>('DB'),
-        synchronize: configService.get<boolean>('SYNC'),
-        autoLoadEntities: configService.get<boolean>('AUTO'),
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        synchronize: configService.get<boolean>('POSTGRES_SYNC'),
+        autoLoadEntities: configService.get<boolean>('POSTGRES_AUTO'),
       }),
       inject: [ConfigService],
     }),
   ],
   exports: [ConfigModule, TypeOrmModule],
 })
-export class DatabaseConfigMod {}
+export class DatabaseConfigureModule {}
